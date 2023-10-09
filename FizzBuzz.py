8ce71717016f76f0ecaaf4f7e4476bd2dddf5789
nisya92 <yaniskia@outlook.fr>
2023-10-09 21:59:04 +0200

    Creation de la deuxieme evolution du code FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 class FizzBuzz():
-    def affiche(self):
+    def affiche(self, n):
         liste = ""
-        for i in range(1, 101):
+        for i in range(1, n+1):
             if i % 15 == 0:
                 liste += "FrisBee"
             elif i % 3 == 0:
