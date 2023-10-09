389e9c7ebb6c1a8ea6ab8b205e49ea2e174c60ea
nisya92 <yaniskia@outlook.fr>
2023-10-09 22:04:07 +0200

    version 1 du test correct avec deux paramètres

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -6,8 +6,9 @@ class TestFizzBuzz(unittest.TestCase):
          self.instance=FizzBuzz()
 
      def test_affiche_sans_param(self):
-         nombre = 15
-         self.assertEqual(self.instance.affiche(nombre), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
+         nombre1 = 5
+         nombre2 = 10
+         self.assertEqual(self.instance.affiche(nombre1, nombre2), "BuzzFizz78FizzBuzz")
 
 if __name__ == '__main__':
      unittest.main()
