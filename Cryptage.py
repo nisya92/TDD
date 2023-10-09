372c14bf9c85e80b876c1851c6e7258a3224a5c8
nisya92 <yaniskia@outlook.fr>
2023-10-09 22:15:48 +0200

    correction de boucle+creation de l'objet Cryptage

--- Cryptage.py
+++ Cryptage.py
@@ -1,15 +1,16 @@
 import string
 
-def crypt(message):
-    caracteres = string.ascii_letters + string.punctuation + string.digits + " "
-    message_crypte = ""
+class Cryptage():
+    def crypt(message):
+        caracteres = string.ascii_letters + string.punctuation + string.digits + " "
+        message_crypte = ""
 
-    for lettre in message:
-        if lettre in caracteres:
-            index = caracteres.index(lettre)
-            lettre_cryptee = caracteres[(index + 1) % len(caracteres)]
-            message_crypte += lettre_cryptee
-        else:
-            message_crypte += lettre
+        for lettre in message:
+            if lettre in caracteres:
+                index = caracteres.index(lettre)
+                lettre_cryptee = caracteres[(index + 1) % len(caracteres)]
+                message_crypte += lettre_cryptee
+            else:
+                message_crypte += lettre
 
-    return message_crypte
+        return message_crypte
