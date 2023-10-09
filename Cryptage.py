c68222fc8793a987f51b8f77d6a0529067682aa1
nisya92 <yaniskia@outlook.fr>
2023-10-09 22:28:57 +0200

    Creation de la première évolution du code cryptage

--- Cryptage.py
+++ Cryptage.py
@@ -1,16 +1,17 @@
 import string
 
 class Cryptage:
-    def crypt(self, message):
+    def crypt(self, message, pas):
         caracteres = string.ascii_letters + string.punctuation + string.digits + " "
         message_crypte = ""
 
         for lettre in message:
             if lettre in caracteres:
                 index = caracteres.index(lettre)
-                lettre_cryptee = caracteres[(index + 1) % len(caracteres)]
+                lettre_cryptee = caracteres[(index + pas) % len(caracteres)]
                 message_crypte += lettre_cryptee
             else:
                 message_crypte += lettre
 
+        message_crypte += str(pas)
         return message_crypte
