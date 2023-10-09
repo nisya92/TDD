b71da035755bac08ee543ed24e1c47c1a6881e8d
nisya92 <yaniskia@outlook.fr>
2023-10-09 22:13:02 +0200

    création de la boucle crypter

--- Cryptage.py
+++ Cryptage.py
@@ -3,3 +3,13 @@ import string
 def crypt(message):
     caracteres = string.ascii_letters + string.punctuation + string.digits + " "
     message_crypte = ""
+
+    for lettre in message:
+        if lettre in caracteres:
+            index = caracteres.index(lettre)
+            lettre_cryptee = caracteres[(index + 1) % len(caracteres)]
+            message_crypte += lettre_cryptee
+        else:
+            message_crypte += lettre
+
+    return message_crypte
