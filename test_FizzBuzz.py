74ea44b0b3eb3302db6d72742360127ae0b7ac33
nisya92 <yaniskia@outlook.fr>
2023-10-09 21:21:18 +0200

    Configuration du fichier test_FizzBuzz

---
+++ test_FizzBuzz.py
@@ -0,0 +1,9 @@
+importer  un test unitaire
+depuis  l'import FizzBuzz  * 
+
+classe  TestFizzBuzz ( unittest . TestCase ):
+     configuration def ( soi ):
+         soi . instance = FizzBuzz ()
+
+si  __name__  ==  'principal' :
+     test unitaire . principal ()
